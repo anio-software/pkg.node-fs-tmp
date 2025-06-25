6c4ff1105e9b400fdf0b3113a4772d10c8b34508
eXory2024 <exory@anio.software>
2025-06-25 22:54:56 +0200

    - allow file mode to be changed. - use options object.

--- project/src/__tmpdirXXX.as.ts
+++ project/src/__tmpdirXXX.as.ts
@@ -6,6 +6,9 @@ import {
 import {mkdir} from "@anio-software/pkg-private.node-consistent-fs/async"
 //>import {mkdir} from "@anio-software/pkg-private.node-consistent-fs/sync"
 
+import type {CommonOptions} from "#~export/CommonOptions.ts"
+
+import {isString, isNumber} from "@anio-software/pkg.is"
 import {randomUUID} from "node:crypto"
 import path from "node:path"
 import os from "node:os"
@@ -13,22 +16,34 @@ import os from "node:os"
 export async function __implementation(
 //>export function __implementationSync(
 	contextOptions: EnkoreJSRuntimeContextOptions,
-	tmpDir?: string
+	options?: CommonOptions|undefined
 ) : Promise<string> {
 //>) : string {
+	const tmpDir: string = (() => {
+		if (isString(options?.tmpDir)) {
+			return options.tmpDir
+		}
+
+		return os.tmpdir()
+	})()
+
+	const fileMode: number = (() => {
+		if (isNumber(options?.mode)) {
+			return options.mode
+		}
+
+		return 0o755
+	})()
+
 	const context = createContext(contextOptions, 0)
 
-	if (tmpDir === undefined) {
-		tmpDir = os.tmpdir()
-	}
-
-	context.log.trace(`using temporary directory '${tmpDir}'`)
+	context.log.trace(`using temporary directory '${tmpDir}' and mode '${fileMode}'`)
 
 	const rand = randomUUID({disableEntropyCache: true})
 	const dirPath = path.join(tmpDir, rand)
 
-	await mkdir(dirPath, {mode: 0o755})
-//>	mkdir(dirPath, {mode: 0o755})
+	await mkdir(dirPath, {mode: fileMode})
+//>	mkdir(dirPath, {mode: fileMode})
 
 	return dirPath
 }
